e9fd03f6d299465c7a733e643224da96e0bd4b19
javierpantoja1998 <cientificodebanco@gmail.com>
2023-05-28 20:17:53 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,59 @@
+﻿using ApiPersonajesAWS.Models;
+using ApiPersonajesAWS.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPersonajesAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private RepositoryPersonajes repo;
+
+        public PersonajesController(RepositoryPersonajes repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Personaje>>> GetPersonajes()
+        {
+            return await this.repo.GetPersonajesAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personaje>> FindPersonaje(int id)
+        {
+            return await this.repo.FindPersonajeAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreatePersonaje(Personaje personaje)
+        {
+            await this.repo.CreatePersonaje(personaje);
+            return Ok();
+        }
+
+        [HttpPut("{id}/{nombre}/{imagen}")]
+        public async Task<ActionResult> UpdatePersonaje(int id, string nombre, string imagen)
+        {
+            await this.repo.UpdatePersonajeProcedure(id, nombre, imagen);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeletePersonaje(int id)
+        {
+            await this.repo.DeletePersonajeProcedure(id);
+            return Ok();
+        }
+
+        [HttpPost("{nombre}/{imagen}")]
+        public async Task<ActionResult> InsertPersonaje(string nombre, string imagen)
+        {
+            await this.repo.InsertPersonajeAsync(nombre, imagen);
+            return Ok();
+        }
+    }
+}
---
+++ ApiPersonajesAWS/Repositories/RepositoryPersonajes.cs
@@ -0,0 +1,133 @@
+﻿using ApiPersonajesAWS.Data;
+using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using System.Data;
+
+#region PROCEDURES
+
+/*
+DELIMITER $$
+CREATE PROCEDURE SP_UPDATE_PERSONAJE (id INT, nombre NVARCHAR(21845), imagen NVARCHAR(21845))
+BEGIN
+    UPDATE PERSONAJES SET PERSONAJE = personaje, IMAGEN = imagen WHERE IDPERSONAJE = id;
+END$$
+
+DELIMITER $$
+CREATE PROCEDURE SP_INSERT_PERSONAJE (nombre NVARCHAR(21845), imagen NVARCHAR(21845))
+BEGIN
+	DECLARE MAX_ID INT;
+    SET MAX_ID = (SELECT (max(IDPERSONAJE)) FROM PERSONAJES) + 1;
+	INSERT PERSONAJES VALUES (MAX_ID,nombre,imagen);
+END$$
+
+DELIMITER $$
+CREATE PROCEDURE SP_DELETE_PERSONAJE (id INT)
+BEGIN
+	DELETE FROM PERSONAJES WHERE ID = id;
+END$$
+*/
+
+#endregion
+
+namespace ApiPersonajesAWS.Repositories
+{
+    public class RepositoryPersonajes
+    {
+        private PersonajesContext context;
+
+        private MySqlConnection conn;
+        private MySqlCommand cmd;
+        private string connectionString;
+
+        public RepositoryPersonajes(PersonajesContext context, IConfiguration config)
+        {
+            this.context = context;
+            this.cmd = new MySqlCommand();
+            this.conn = new MySqlConnection(config.GetConnectionString("MySqlTelevision"));
+            
+        }
+
+        private int GetMaxIdPersonaje()
+        {
+            if (this.context.Personajes.Count() != 0)
+            {
+                return this.context.Personajes.Max(x => x.IdPersonaje) + 1;
+            }
+
+            return 1;
+        }
+        
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            return await this.context.Personajes.ToListAsync();
+        }
+
+        public async Task<Personaje> FindPersonajeAsync(int id)
+        {
+            return await this.context.Personajes.FirstOrDefaultAsync(x => x.IdPersonaje == id);
+        }
+
+        public async Task CreatePersonaje(Personaje personaje)
+        {
+            personaje.IdPersonaje = this.GetMaxIdPersonaje();
+            this.context.Personajes.Add(personaje);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task UpdatePersonajeProcedure(int id, string nombre, string imagen)
+        {
+            this.conn.Open();
+            this.cmd.Connection = this.conn;
+
+            this.cmd.CommandText = "SP_UPDATE_PERSONAJE";
+            this.cmd.CommandType = CommandType.StoredProcedure;
+
+            this.cmd.Parameters.AddWithValue("@id", id);
+            this.cmd.Parameters.AddWithValue("@nombre", nombre);
+            this.cmd.Parameters.AddWithValue("@imagen", imagen);
+
+            await this.cmd.ExecuteNonQueryAsync();
+
+            this.cmd.Parameters.Clear();
+            this.conn.Close();
+            
+        }
+
+        public async Task InsertPersonajeAsync(string nombre, string imagen)
+        {
+            this.conn.Open();
+            this.cmd.Connection = this.conn;
+
+            this.cmd.CommandText = "SP_INSERT_PERSONAJE";
+            this.cmd.CommandType = CommandType.StoredProcedure;
+
+            this.cmd.Parameters.AddWithValue("@nombre", nombre);
+            this.cmd.Parameters.AddWithValue("@imagen", imagen);
+
+            await this.cmd.ExecuteNonQueryAsync();
+
+            this.cmd.Parameters.Clear();
+            this.conn.Close();
+
+        }
+
+        public async Task DeletePersonajeProcedure(int id)
+        {
+            this.conn.Open();
+            this.cmd.Connection = this.conn;
+
+            this.cmd.CommandText = "SP_DELETE_PERSONAJE";
+            this.cmd.CommandType = CommandType.StoredProcedure;
+
+            this.cmd.Parameters.AddWithValue("@id", id);
+
+            await this.cmd.ExecuteNonQueryAsync();
+
+            this.cmd.Parameters.Clear();
+            this.conn.Close();
+        }
+
+
+    }
+}
